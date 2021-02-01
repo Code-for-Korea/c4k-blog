198fdbdc970bca41c88b8dac266df095ef87b139
Taeyang Jin <sun@jupiterflow.com>
2021-02-02 07:15:30 +0900

    :bug: fixed test properties

--- editor/src/main/java/kr/codefor/blog/EditorApplication.java
+++ editor/src/main/java/kr/codefor/blog/EditorApplication.java
@@ -1,6 +1,5 @@
 package kr.codefor.blog;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
--- editor/src/test/java/kr/codefor/blog/EditorApplicationTests.java
+++ editor/src/test/java/kr/codefor/blog/EditorApplicationTests.java
@@ -3,11 +3,15 @@ package kr.codefor.blog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "" +
+        "spring.config.location=" +
+        "classpath:/application.yml," +
+        "optional:C:/repository/_secrets/c4k-blog.yml," +
+        "optional:/home/ec2-user/_secrets/c4k-blog.yml")
 class EditorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
