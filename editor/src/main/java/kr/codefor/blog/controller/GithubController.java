bdce22c7010d5e000a0c16286ca622831a2b6de1
Taeyang Jin <sun@jupiterflow.com>
2021-02-07 02:51:17 +0900

    Update GithubController.java

--- editor/src/main/java/kr/codefor/blog/controller/GithubController.java
+++ editor/src/main/java/kr/codefor/blog/controller/GithubController.java
@@ -125,7 +125,7 @@ public class GithubController {
             Session one = sessionService.findOne(saveId);
             result.put("session_id", one.getSessionId());
             result.put("refresh_token", one.getRefreshToken());
-            result.put("TEST1", "QQQQ");
+            result.put("TEST1", "FFF");
             return new JSONResponse(HttpStatus.OK, result);
         }
         return new JSONResponse(HttpStatus.BAD_REQUEST, resultMap);
