d6464bbd628c8c9f62126cf210ce66293b855f1b
Taeyang Jin <sun@jupiterflow.com>
2021-02-09 02:10:23 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-BLOG_REPOSITORY=/home/ec2-user/c4k-blog/front
-cd BLOG_REPOSITORY
-
-sudo docker run -i \
-jekyll/jekyll \
-jekyll build
+#BLOG_REPOSITORY=/home/ec2-user/c4k-blog/front
+#cd BLOG_REPOSITORY
+#
+#sudo docker run -i \
+#jekyll/jekyll \
+#jekyll build
