5a26fd797130bf4dc480d533c41d7ff901c786f5
Taeyang Jin <sun@jupiterflow.com>
2021-02-09 01:17:14 +0900

    update appspec

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-BLOG_REPOSITORY=/home/ec2-user/c4k-blog/front
+BLOG_REPOSITORY=/home/ec2-user/c4k-blog-front
 cd BLOG_REPOSITORY
 
 sudo docker run --volume="$PWD:/srv/jekyll" jekyll/jekyll jekyll build
