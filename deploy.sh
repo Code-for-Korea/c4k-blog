5757f4227cf8c7a47902a7fad935e3bd5f246dad
Taeyang Jin <sun@jupiterflow.com>
2021-02-09 01:46:05 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ cd BLOG_REPOSITORY
 
 sudo docker run \
 -v $PWD/:/srv/jekyll -v $PWD/_site:/srv/jekyll/_site \
-jekyll/builder:latest /bin/bash -c "chmod 777 /srv/jekyll && jekyll build"
+jekyll/builder:latest /bin/bash -c "chmod 755 /srv/jekyll && jekyll build"
